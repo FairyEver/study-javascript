31e7d5cb9d2deb4ea45cc9d7c05c68ed88825ce8
李杨 <1711467488@qq.com>
2017-12-27 22:44:02 +0800

    no message

--- let-and-const/5-不允许重复声明.js
+++ let-and-const/5-不允许重复声明.js
@@ -2,9 +2,14 @@
 // let a = 2
 // SyntaxError: Identifier 'a' has already been declared
 
+function test() {
+    var a = 1
+    let a = 2
+}
+// test() // SyntaxError: Identifier 'a' has already been declared
+
 function demo(name) {
-    // let name = 1
-    // SyntaxError: Identifier 'name' has already been declared
+    let name = 1
 }
 
-demo()
+// demo() // SyntaxError: Identifier 'name' has already been declared
