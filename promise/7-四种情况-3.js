6b603f472a76fd9c6386b5482e7c9f08e3f7c3ee
李杨 <1711467488@qq.com>
2018-04-02 08:34:56 +0800

    no message

--- promise/7-四种情况-3.js
+++ promise/7-四种情况-3.js
@@ -4,15 +4,12 @@ new Promise((resolve, reject) => {
     resolve('Hello1')
   }, 1000)
 })
-  .then((val) => {
-    console.log(val)
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log('step2 end')
-        resolve('Hello2')
-      }, 1000)
-    })
-  })
+  .then(new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('step2 end')
+      resolve('Hello2')
+    }, 2000)
+  }))
   .then((val) => {
     console.log(val)
     console.log('step3 end')
@@ -20,6 +17,5 @@ new Promise((resolve, reject) => {
 
 // step1 end
 // Hello1
-// undefined
 // step3 end
 // step2 end
