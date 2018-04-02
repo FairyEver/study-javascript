bf40192e26b9a9e7b30bb8613b6b9c9887d322e8
李杨 <1711467488@qq.com>
2018-04-02 22:22:04 +0800

    no message

--- promise/11-resolve.js
+++ promise/11-resolve.js
@@ -7,4 +7,23 @@ p
   })
   .then(value => {
     console.log('step1 return is : ', value)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('step2 return value')
+      }, 2000)
+    })
   })
+  .then(value => {
+    console.log('step2 return is : ', value)
+    return {
+      then () {
+        console.log('thenable')
+      }
+    }
+  })
+
+// step1
+// step1 return is :  step1 return value
+// step2 return is :  step2 return value
+// thenable
+  
