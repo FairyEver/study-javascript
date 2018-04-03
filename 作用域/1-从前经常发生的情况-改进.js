2be7602835865e7836411ed1e239b6e376136d58
李杨 <1711467488@qq.com>
2018-04-03 08:13:09 +0800

    no message

---
+++ 作用域/1-从前经常发生的情况-改进.js
@@ -0,0 +1,12 @@
+var callbacks = []
+for (let i = 0; i < 5; i++) {
+  callbacks[i] = () => {
+    console.log(i)
+  }
+}
+
+// console.log(i) // i is not defined
+callbacks[0]() // 0
+callbacks[1]() // 1
+callbacks[2]() // 2
+callbacks[3]() // 3
