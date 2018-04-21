dcaf20fb7d6e7bd3452d0c369fd38cd634a831c6
李杨 <1711467488@qq.com>
2018-04-21 18:21:28 +0800

    no message

---
+++ this/1.js
@@ -0,0 +1,27 @@
+const name = 'outter name'
+const obj = {
+  name: 'obj name',
+  sayName () {
+    console.log(this.name)
+  },
+  sayName2: function () {
+    console.log(this.name)
+  },
+  sayName3: () => {
+    console.log(this.name)
+  }
+}
+
+obj.sayName() // obj name
+
+obj.sayName2() // obj name
+
+obj.sayName3() // undefined
+
+
+
+
+
+
+
+
